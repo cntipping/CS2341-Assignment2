c54b9e79bc5bfd07c08476ca40a1894228cf5258
cntipping <167351806+cntipping@users.noreply.github.com>
2024-10-28 23:15:00 -0500

    Update Job.java

--- Job.java
+++ Job.java
@@ -1,12 +1,32 @@
 public class Job{
-    private int id, processingTime;
+    private int id, processingTime, priority, arrivalTime;
 
+    // Constructor for task1
     public Job(int id, int processingTime){
         this.id = id;
         this.processingTime = processingTime;
+        priority = -1;
+        arrivalTime = -1;
+    }
+
+    // Constructor for task2
+    public Job(int id, int processingTime, int priority){
+        this.id = id;
+        this.processingTime = processingTime;
+        this.priority = priority;
+        arrivalTime = -1;
+    }
+
+    // Constructor for task3
+    public Job(int id, int processingTime, int priority, int arrivalTime){
+        this.id = id;
+        this.processingTime = processingTime;
+        this.priority = priority;
+        this.arrivalTime = arrivalTime;
     }
 
     public int getId(){ return id;}
-
     public int getProcessingTime(){return processingTime;}
+    public int getPriority(){return priority;}
+    public int getArrivalTime(){return arrivalTime;}
 }
