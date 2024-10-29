b2ce7b7add5c1fe5d03c495d3eb8f20a74bd8d05
cntipping <167351806+cntipping@users.noreply.github.com>
2024-10-28 23:15:23 -0500

    Update Task1Scheduler.java

--- Task1Scheduler.java
+++ Task1Scheduler.java
@@ -4,7 +4,7 @@ import java.io.*;
 public class Task1Scheduler{
 
     public static void main(String[] args){
-        List<Job> jobs = readJobFile("task1-input.txt");
+        List<Job> jobs = JobReader.jobReader("task1-input.txt");
 
         PriorityQueue<Job> pq = new PriorityQueue<>(new Comparator<Job>() {
             @Override
@@ -18,7 +18,7 @@ public class Task1Scheduler{
         List<Integer> executionOrder = new ArrayList<>();
         int totalTime = 0, completionTime = 0;
 
-        while(!pq.isEmpty()){
+       while(!pq.isEmpty()){
             Job job = pq.poll();
             completionTime += job.getProcessingTime();
             totalTime += completionTime;
@@ -31,21 +31,5 @@ public class Task1Scheduler{
         System.out.println("Average completion time: " + averageCompletionTime);
 
     }
-
-    public static List<Job> readJobFile(String fileName){
-
-        List<Job> jobs = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
-            String line;
-            while ((line = br.readLine()) != null){
-                String[] parts = line.split(" ");
-                int id = Integer.parseInt(parts[0]);
-                int processingTime = Integer.parseInt(parts[1]);
-                jobs.add(new Job(id, processingTime));
-            }
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-        return jobs;
-    }
 }
+
